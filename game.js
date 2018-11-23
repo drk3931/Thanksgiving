8a3ad74709b84e4ae33ae4c143de420c88fdc658
Deepak K <drk3931@gmail.com>
2018-11-23 12:43:44 -0500

    added meme pulls

--- game.js
+++ game.js
@@ -1,59 +1,67 @@
 "use strict";
 
-console.log("LOADED");
+console.log("LOADED game.js");
 
-var sun = new Image();
-var moon = new Image();
-var earth = new Image();
 const W = 1000;
-const H = 700
+const H = 700;
+const MEME_HEIGHT = 250;
+const MEME_WIDTH = 210;
+const MEME_COUNT = 7;
 
 var ctx = document.getElementById("AppCanvas").getContext("2d");
+var memes = [];
+var loaded = false;
 
-function init() {
-  ctx.scale(3.5,2.5);
-
-  sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
-  moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
-  earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
-  window.requestAnimationFrame(draw);
+function onMemeRetrieval() {
+  let response = this;
+  let jsonResponse = JSON.parse(response.responseText);
+  console.log(jsonResponse);
+  jsonResponse.data.memes.forEach(
+    meme => 
+    {
+    let img = new Image(MEME_WIDTH, MEME_HEIGHT);
+    img.src = meme.url;
+    memes.push(img);
+    //console.log(img.src);
+    //console.log(memes[jsonResponse.data.memes.indexOf(meme)]);
+    }
+  );
+  loaded = true;
+  setTimeout(draw,1000);
+  //draw();
 }
 
+function init() {
+
+  var memeReq = new XMLHttpRequest();
+  memeReq.addEventListener("load", onMemeRetrieval);
+  memeReq.open("GET", "https://api.imgflip.com/get_memes", true);
+  memeReq.send();
+
+}
+
+var earth = new Image();
+earth.src = "https://mdn.mozillademos.org/files/1429/Canvas_earth.png";
+
 function draw() {
+
   
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.clearRect(0, 0, 300, 300); // clear canvas
-  
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-    ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
-    ctx.save();
-    ctx.translate(150, 150);
-  
-    // Earth
-    var time = new Date();
-    ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
-    ctx.translate(105, 0);
-    ctx.fillRect(0, -12, 40, 24); // Shadow
-    ctx.drawImage(earth, -12, -12);
-  
-    // Moon
-    ctx.save();
-    ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-    ctx.translate(0, 28.5);
-    ctx.drawImage(moon, -3.5, -3.5);
-    ctx.restore();
-  
-    ctx.restore();
-    
-    ctx.beginPath();
-    ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-    ctx.stroke();
-   
-    ctx.drawImage(sun, 0, 0, 300, 300);
-  
-    window.requestAnimationFrame(draw);
+  ctx.globalCompositeOperation = 'destination-over';
+
+  //console.log("DRAWING");
+
+  for (let i = 0; i < 10; i++) {
+    //ctx.drawImage(memes[i],i,0);
+    //ctx.drawImage(earth,i,20);
+    let randIndex =  parseInt( (Math.random() * 100 ) % memes.length );
+    console.log(randIndex);
+    ctx.drawImage(memes[randIndex],MEME_WIDTH*i,0,MEME_WIDTH,MEME_HEIGHT);
+
   }
 
-  init();
-  
+
+}
+
+init();
+
 
